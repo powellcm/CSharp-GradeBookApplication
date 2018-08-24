8f849a4a5545b325249ee0418dcf3bf98c570546
Chris Powell <chris.powell@cdk.com>
2018-08-24 17:25:34 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,38 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            double gradeBoundry;
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            gradeBoundry = Students.Count * 0.2;
+
+            if (averageGrade >= gradeBoundry * 4)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= gradeBoundry * 3)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= gradeBoundry * 2)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= gradeBoundry * 1)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
